1be73a83e9b979f21cbc0e252a16359a74febf24
Sumon Deb <sumon.deb@sslwireless.com>
2024-02-14 13:34:03 +0600

    Dashboard design update

--- config/admin.php
+++ config/admin.php
@@ -58,7 +58,7 @@ return [
     */
     'route' => [
 
-        'prefix' => env('ADMIN_ROUTE_PREFIX', 'admin'),
+        'prefix' => env('ADMIN_ROUTE_PREFIX', 'hack-fit'),
 
         'namespace' => 'App\\Admin\\Controllers',
 
@@ -85,7 +85,7 @@ return [
     | Html title for all pages.
     |
     */
-    'title' => 'Admin',
+    'title' => 'Hack Fit',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/admin/dashboard/quote.blade.php
+++ resources/views/admin/dashboard/quote.blade.php
@@ -1,7 +1,7 @@
 <div class="box box-info" style="padding: 5px;border-bottom: 3px solid #d2d6de;">
-    <div class="box-header with-border text-center">
+    <div class="box-body with-border text-center">
         <h3 class="box-title" style="font-weight: bold;opacity: 0.8;font-size: 22px;">
-            <p style="font-style: italic;margin: 15px;"><q>{{$quote}}</q> - {{$author}}</p>
+            <p style="font-style: italic;margin: 0 15px 15px; font-weight: bold;opacity: 0.8;font-size: 22px;"><q>{{$quote}}</q> - {{$author}}</p>
         </h3>
     </div>
 </div>
